bf8779f0e4f49db1b918b585095c458a1101ace6
nemethkoppany <koppany344@gmail.com>
2026-01-05 22:27:28 +0100

    Még egy teszteset

--- vizsgafeladat/nemeth_koppany/cypress/e2e/api.cy.ts
+++ vizsgafeladat/nemeth_koppany/cypress/e2e/api.cy.ts
@@ -1,10 +1,19 @@
 describe("Backend API tests", () => {
-
   it("GET /api/auto returns 200 and array", () => {
     cy.request("GET", "/api/auto").then((res) => {
-      expect(res.status).to.eq(200)
-      expect(res.body).to.be.an("array")
-    })
-  })
+      expect(res.status).to.eq(200);
+      expect(res.body).to.be.an("array");
+    });
+  });
 
-})
+  it("Verify that GET kolcsonzes has token", () => {
+    cy.request({
+      method: "GET",
+      url: "/api/kolcsonzes/auto/1",
+      failOnStatusCode:false,
+      body:{datum:"2026-01-05",napokszam:2}
+    }).then((res) => {
+      expect(res.status).to.eq(403);
+    });
+  });
+});
